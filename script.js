8dde9d6acbd5cc3275d09701c0503736a92721e4
Abdallah <abdallahchaar90@gmail.comh>
2020-12-04 09:49:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const result = 10 + 10 + 10;
+const result2 = 5 + 5 + 5;
+
+const averageResult = result / 2;
+const averageResult2 = result2 / 2;
+
+//console.log(result, result2);
+//console.log(averageResult, averageResult2);
+
+if (averageResult > averageResult2){
+    console.log("GRATTIS OG");
+} else{
+    console.log("GRATTIS ALLIANCE")
+}
+
+
